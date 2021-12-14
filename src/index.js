a0b2d74d96e19e1c5b68c0a2aabb95765fdea9ec
Kushagra Singh Bisen <k.bisen@yahoo.com>
2021-12-14 21:08:48 +0100

    done with react components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./components/CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./components/ApprovalCard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>WARNING!</h4>Are you sure you want to approve this?
+        </div>
+      </ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <ApprovalCard>
+        <CommentDetail
+          author="Kushagra"
+          timeAgo="Today at 10:00"
+          content="Great!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="John"
+          timeAgo="Today at 13:00"
+          content="Okayish"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Pranshu"
+          timeAgo="Yesterday at 15:00"
+          content="Not that great"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sting"
+          timeAgo="Today at 16:00"
+          content="Could have been better"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Undertaker"
+          timeAgo="Yesterday at 12:00"
+          content="Thanks for posting"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
